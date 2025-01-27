6e952668a491782e1e3c16f72d62fcef8b8bf4c8
1A-engineer <drewuncle863724lu@gmail.com>
2025-01-27 12:28:00 +0800

    set up

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function Home() {
+  return (
+    <>
+      <Button>test</Button>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+    </>
+  );
+}
