2a2bdcab2321c006eb3dfbfe01e79ac079061375
1A-engineer <drewuncle863724lu@gmail.com>
2025-01-30 10:33:56 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,51 +9,45 @@ import { ImageShiftPart } from "../components/CarouselPart";
 import axios from "axios";
 import { useEffect } from "react";
 
-
 const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
 const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export default function Home() {
-
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [popularMovieData, setPopularMovieData] = useState([]);
 
-const getMovieData = async () => {
-  try {
-    setIsLoading(true);
-console.log("this is running");
-const response = await axios.get(
-  `${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
-  { 
-    headers: {
-      Authorization: `Bearer ${TMDB_API_TOKEN}`,
-    },
-  }
-);
-setPopularMovieData(response.data.results);
-setIsLoading(false);
-  } catch (err) {
-    setIsLoading(false);
-    if(axios.isAxiosError(err)) {
-      setErrorMessage(err.response?.data.statusmessage_message);
+  const getMovieData = async () => {
+    try {
+      setIsLoading(true);
+      console.log("this is running");
+      const response = await axios.get(
+        `${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer ${TMDB_API_TOKEN}`,
+          },
+        }
+      );
+      setPopularMovieData(response.data.results);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      if (axios.isAxiosError(err)) {
+        setErrorMessage(err.response?.data.statusmessage_message);
+      }
+      console.log(err);
     }
-    console.log(err);
+  };
+  console.log("This is the error", errorMessage);
+  console.log(isLoading);
 
-  }
-};
-console.log("This is the error",errorMessage);
-console.log(isLoading);
+  console.log("This is the data", popularMovieData);
 
-console.log("This is the data",popularMovieData);
-
-
-  
-  useEffect(() => { 
+  useEffect(() => {
     getMovieData();
   }, []);
-
 
   return (
     <>
@@ -72,10 +66,10 @@ console.log("This is the data",popularMovieData);
                   See more
                 </p>
                 <RightArrow />
-              </div> 
+              </div>
             </div>
             <div className="MovieImgs w-100% h-auto mt-[20px]">
-              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  xl:grid-cols-5 gap-4 mt-6 lg:gap-[30px] xl:gap-[30px]">
+              <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4  xl:grid-cols-5 gap-4 mt-6 2xl:gap-[30px] ">
                 <div className="w-[175px] h-[233px] bg-[#E4E4E7] rounded-[8px] lg:w-[230px] lg:h-[440px]"></div>
                 <div className="w-[175px] h-[233px] bg-[#E4E4E7] rounded-[8px] lg:w-[230px] lg:h-[440px]"></div>
                 <div className="w-[175px] h-[233px] bg-[#E4E4E7] rounded-[8px] lg:w-[230px] lg:h-[440px]"></div>
@@ -87,20 +81,15 @@ console.log("This is the data",popularMovieData);
                 <div className="w-[175px] h-[233px] bg-[#E4E4E7] rounded-[8px] lg:w-[230px] lg:h-[440px]"></div>
                 <div className="w-[175px] h-[233px] bg-[#E4E4E7] rounded-[8px] lg:w-[230px] lg:h-[440px]"></div>
               </div>
-             
             </div>
           </div>
           <div className="MovieList w-100% h-auto px-[20px] flex flex-col mt-[32px]">
             <div className="w-100% h-[36px] flex flex-row justify-between">
               <div className="w-auto h-100% flex justify-center items-center">
-                <p className="text-[24px] font-semibold ">
-                  Top Rated
-                </p>
+                <p className="text-[24px] font-semibold ">Top Rated</p>
               </div>
               <div className="w-[120px] h-100% flex flex-row justify-center items-center gap-[8px]">
-                <p className="text-[14px] font-medium">
-                  See more
-                </p>
+                <p className="text-[14px] font-medium">See more</p>
                 <RightArrow />
               </div>
             </div>
@@ -109,14 +98,10 @@ console.log("This is the data",popularMovieData);
           <div className="MovieList w-100% h-auto px-[20px] flex flex-col mt-[32px]">
             <div className="w-100% h-[36px] flex flex-row justify-between">
               <div className="w-auto h-100% flex justify-center items-center">
-                <p className="text-[24px] font-semibold ">
-                  Popular
-                </p>
+                <p className="text-[24px] font-semibold ">Popular</p>
               </div>
               <div className="w-[120px] h-100% flex flex-row justify-center items-center gap-[8px]">
-                <p className="text-[14px] font-medium">
-                  See more
-                </p>
+                <p className="text-[14px] font-medium">See more</p>
                 <RightArrow />
               </div>
             </div>
--- src/components/CarouselPart.tsx
+++ src/components/CarouselPart.tsx
@@ -1,13 +1,55 @@
 
 import { Button } from "./ui/button";
 import Starx from "@/app/icons/Star";
+import { useState, useEffect } from "react";
+import axios
+ from "axios";
 
+
+ const TMDB_BASE_URL = process.env.TMDB_BASE_URL;
+ const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+ const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 export const ImageShiftPart = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [popularMovieData, setPopularMovieData] = useState([]);
+
+  const getMovieData = async () => {
+    try {
+      setIsLoading(true);
+      console.log("this is running");
+      const response = await axios.get(
+        `${TMDB_BASE_URL}/movie/popular?api_key=${TMDB_API_KEY}&language=en-US&page=1`,
+        {
+          headers: {
+            Authorization: `Bearer ${TMDB_API_TOKEN}`,
+          },
+        }
+      );
+      setPopularMovieData(response.data.results);
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      if (axios.isAxiosError(err)) {
+        setErrorMessage(err.response?.data.statusmessage_message);
+      }
+      console.log(err);
+    }
+  };
+  console.log("This is the error", errorMessage);
+  console.log(isLoading);
+  console.log("This is the data", popularMovieData);
+
+
+  useEffect(() => {
+    getMovieData();
+  }, []);
+
   return (
     <div className="w-screen h-auto flex flex-col lg:relative">
       <div className="ImageContainer w-full h-[246px] flex justify-center items-center lg:h-[600px]">
-      here goes image
+
       </div>
       <div className="AboutMovie w-full h-auto px-[20px] py-[20px] flex flex-col gap-[16px] justify-content lg:absolute lg:top-[40%] lg:left-[10%] lg:w-[302px] lg:h-[246px] lg:rounded-[8px] lg:bg-transparent">
         <div className="w-full h-auto">
@@ -41,3 +83,4 @@ export const ImageShiftPart = () => {
     </div>
   );
 };
+
